8401c97679b7b45464db266c1ce0893ac4a33371
Eulogio Enamorado Pallares <eulogio-ep@hotmail.com>
2021-10-20 18:44:55 +0200

    Fin del ejercicio

--- EjemploInterface/EjemploInterface/Form1.Designer.cs
+++ EjemploInterface/EjemploInterface/Form1.Designer.cs
@@ -28,12 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtBox = new System.Windows.Forms.TextBox();
+            this.btnMuestra = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBox
+            // 
+            this.txtBox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtBox.Location = new System.Drawing.Point(12, 12);
+            this.txtBox.Multiline = true;
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(230, 183);
+            this.txtBox.TabIndex = 0;
+            // 
+            // btnMuestra
+            // 
+            this.btnMuestra.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnMuestra.Location = new System.Drawing.Point(274, 12);
+            this.btnMuestra.Name = "btnMuestra";
+            this.btnMuestra.Size = new System.Drawing.Size(102, 40);
+            this.btnMuestra.TabIndex = 1;
+            this.btnMuestra.Text = "Muestra";
+            this.btnMuestra.UseVisualStyleBackColor = true;
+            this.btnMuestra.Click += new System.EventHandler(this.btnMuestra_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(515, 415);
+            this.Controls.Add(this.btnMuestra);
+            this.Controls.Add(this.txtBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtBox;
+        private System.Windows.Forms.Button btnMuestra;
     }
 }
--- EjemploInterface/EjemploInterface/Form1.cs
+++ EjemploInterface/EjemploInterface/Form1.cs
@@ -17,5 +17,57 @@ namespace EjemploInterface
             InitializeComponent();
         }
 
+        interface Ordenador
+        {
+            void GetEscritorio();
+            void GetPortatil();
+        }
+
+        interface DellComputer
+        {
+            void GetMark();
+        }
+
+        class OrdenadoresDell : Ordenador, DellComputer
+        {
+            public string Nombre { get; set; }
+            public double Precio { get; set; }
+            public string Mark { get; set; }
+
+            public void GetEscritorio()
+            {
+                Nombre = "Escritorio1";
+                Precio = 2000;
+            }
+
+            public void GetPortatil()
+            {
+                Nombre = "Portátil1";
+                Precio = 1500;
+            }
+
+            public void GetMark()
+            {
+                Mark = "Dell";
+            }
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMuestra_Click(object sender, EventArgs e)
+        {
+            OrdenadoresDell dell = new OrdenadoresDell();
+            dell.GetPortatil();
+            dell.GetMark();
+            string valorTexto = "Nombre: " + dell.Nombre + Environment.NewLine;
+            valorTexto += "Precio: " + dell.Precio + Environment.NewLine;
+            valorTexto += "Marca: " + dell.Mark + Environment.NewLine;
+            txtBox.Text = valorTexto;
+        }
     }
 }
