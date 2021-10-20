09f3543f41efeef5c08c1cce77bc3bfe10df4122
Eulogio Enamorado Pallares <eulogio-ep@hotmail.com>
2021-10-20 23:15:50 +0200

    Fin Ejemplo Clases Abstractas

--- EjemploClasesAbstractas/EjemploClasesAbstractas/Form1.Designer.cs
+++ EjemploClasesAbstractas/EjemploClasesAbstractas/Form1.Designer.cs
@@ -29,13 +29,49 @@ namespace EjemploClasesAbstractas
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.txtBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMostrar.Location = new System.Drawing.Point(562, 12);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(111, 42);
+            this.btnMostrar.TabIndex = 0;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // txtBox
+            // 
+            this.txtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBox.Location = new System.Drawing.Point(12, 12);
+            this.txtBox.Multiline = true;
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(530, 295);
+            this.txtBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtBox);
+            this.Controls.Add(this.btnMostrar);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.TextBox txtBox;
     }
 }
 
--- EjemploClasesAbstractas/EjemploClasesAbstractas/Form1.cs
+++ EjemploClasesAbstractas/EjemploClasesAbstractas/Form1.cs
@@ -16,5 +16,54 @@ namespace EjemploClasesAbstractas
         {
             InitializeComponent();
         }
+
+        abstract class Vehiculo
+        {
+            public abstract double GetCantidadCombustible();
+        }
+
+        class Autobus : Vehiculo
+        {
+            public override double GetCantidadCombustible()
+            {
+                return 300;
+            }
+        }
+
+        class Camion : Vehiculo
+        {
+            public override double GetCantidadCombustible()
+            {
+                return 500;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            string texto = "";
+
+            //Al ser una clase abstracta no se puede instanciar
+            Vehiculo vc;
+
+            //Se asigna al objeto vc una instancia de Autobus que hereda de la Clase Abstracta Vehículo
+            vc = new Autobus();
+
+            double combustibleBus = vc.GetCantidadCombustible();
+
+            texto += "La Cantidad de Combustible del Autobus es: " + combustibleBus.ToString() + Environment.NewLine;
+
+            vc = new Camion();
+
+            double combustibleCamion = vc.GetCantidadCombustible();
+
+            texto += "La Cantidad de Combustible del Camión es: " + combustibleCamion.ToString() + Environment.NewLine;
+
+            txtBox.Text = texto;
+        }
     }
 }
