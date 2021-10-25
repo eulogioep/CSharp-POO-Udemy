5262b95e2ed77d45623bb966fef714e1c53f5156
Eulogio Enamorado Pallares <eulogio-ep@hotmail.com>
2021-10-25 19:50:36 +0200

    Ejemplo Clases Partial y Sealed

---
+++ EjemploPartialClass/EjemploPartialClass/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace EjemploPartialClass
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.txtCaja = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMostrar.Location = new System.Drawing.Point(275, 12);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(129, 44);
+            this.btnMostrar.TabIndex = 0;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // txtCaja
+            // 
+            this.txtCaja.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCaja.Location = new System.Drawing.Point(12, 12);
+            this.txtCaja.Multiline = true;
+            this.txtCaja.Name = "txtCaja";
+            this.txtCaja.Size = new System.Drawing.Size(241, 258);
+            this.txtCaja.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtCaja);
+            this.Controls.Add(this.btnMostrar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.TextBox txtCaja;
+    }
+}
+
---
+++ EjemploPartialClass/EjemploPartialClass/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjemploPartialClass
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Las Clases parciales pueden crearse en varias partes
+
+        public partial class Empleado
+        {
+            public int EmpleadoID { get; set; }
+            public string Nombre { get; set; }
+            public double Salario { get; set; }
+        }
+
+        public partial class Empleado
+        {
+            public void SetValues()
+            {
+                Nombre = "Eulogio";
+                EmpleadoID = 1;
+                Salario = 5000;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public sealed class ClaseSellada
+        {
+
+        }
+
+        // Las Clases selladas no pueden ser heredadas
+        //
+        //public class NuevaClase : ClaseSellada
+        //{
+
+        //}
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            Empleado emp = new Empleado();
+            emp.SetValues();
+            txtCaja.Text += emp.EmpleadoID + Environment.NewLine;
+            txtCaja.Text += emp.Nombre + Environment.NewLine;
+            txtCaja.Text += emp.Salario + Environment.NewLine;
+        }
+    }
+}
